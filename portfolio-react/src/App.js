19c57599dc7009e1fbe7b37e5daf54916c9105d5
kaylanerss <kaylane25@hotmail.com>
2022-01-27 13:54:46 -0300

    iniciando app react

---
+++ portfolio-react/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <>
+      <Switch>
+
+      </Switch>
+      </>
+    )
+  }
+}
+
+
+export default App;
