6f11a63b1781872d1d53215116ac2a7fe8f7da5d
kaylanerss <kaylane25@hotmail.com>
2022-02-02 19:39:12 -0300

    movendo arquivo css para o uma pasta de css

--- portfolio-react/src/components/Home.js
+++ portfolio-react/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import MinhaFoto from '../images/minhafoto.jpg'
-import '../Home.css'
+import '../css/Home.css'
 
 class Home extends Component {
   render() {
